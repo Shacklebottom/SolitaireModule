f5b535c8a421c2bd4402dc91af3cd731ca801618
Shacklebottom <last0illusion@gmail.com>
2024-04-08 18:13:14 -0500

    minor refactoring of DeckTests

--- ModuleTesting/DomainObjTests/DeckTests.cs
+++ ModuleTesting/DomainObjTests/DeckTests.cs
@@ -34,7 +34,7 @@ namespace ModuleTesting.DomainObjTests
             //1. Global Initalizer
 
             //Act
-            int? uniqueCardCount = deck.Cards.GroupBy(c => new { c.Rank, c.Suit }).Count();
+            var uniqueCardCount = deck.Cards.GroupBy(c => new { c.Rank, c.Suit }).Count();
 
             //Assert
             Assert.AreEqual(52, uniqueCardCount);
@@ -48,11 +48,11 @@ namespace ModuleTesting.DomainObjTests
 
             //Act
             deck.Shuffle();
-            var firstFiveCards = deck.Cards.Take(5);
-            bool areAllHearts = firstFiveCards.All(c => c.Suit == Suit.Hearts);
-            int sumFirstFive = firstFiveCards.Sum(c => (int)c.Rank);
 
             //Assert
+            var firstFiveCards = deck.Cards.Take(5);
+            var areAllHearts = firstFiveCards.All(c => c.Suit == Suit.Hearts);
+            var sumFirstFive = firstFiveCards.Sum(c => (int)c.Rank);
             //1. If the first five cards are NOT Hearts _and_ the first five cards DON'T sum to 15. If both are true, the deck hasn't been shuffled.
             Assert.IsFalse(areAllHearts && sumFirstFive == 15, "First five cards were Ace - Five of Hearts");
         }
@@ -62,16 +62,17 @@ namespace ModuleTesting.DomainObjTests
         {
             //Arrange
             //1. Global Initalizer
-            List<Card> firstThreeCards = deck.Cards.Take(3).ToList();
+            var cardsToDraw = 3;
+            var firstThreeCards = deck.Cards.Take(cardsToDraw).ToList();
 
             //Act
-            List<Card> cards = deck.Draw(3);
+            var cards = deck.Draw(cardsToDraw);
 
             //Assert
             //1. the drawn cards were the first three cards.
-            Assert.IsTrue(firstThreeCards.SequenceEqual(cards));
+            Assert.IsTrue(firstThreeCards.SequenceEqual(cards), $"The first {cardsToDraw} cards were not the cards drawn");
             //2. the first three cards are no longer in the deck.
-            Assert.IsFalse(cards.Any(deck.Cards.Contains));
+            Assert.IsFalse(cards.Any(deck.Cards.Contains), $"The first {cardsToDraw} cards weren't removed from the deck");
         }
     }
 }
