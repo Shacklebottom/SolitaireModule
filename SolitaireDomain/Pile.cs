ff8685643bc81b5aaa348126912bf221989c54e5
Shacklebottom <last0illusion@gmail.com>
2024-04-23 09:00:49 -0500

    created IHeap interface. Added Pile and Foundation objs that contract with IHeap.

---
+++ SolitaireDomain/Pile.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SolitaireDomain
+{
+    public class Pile : IHeap
+    {
+        public List<Card> Cards { get; set; } = [];
+
+        public bool ValidatePlay()
+        {
+            return true;
+        }
+    }
+}
